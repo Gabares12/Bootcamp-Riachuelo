21ad255399229cdc46549b6c6cce31a3bbdcbee4
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-08-15 11:52:24 -0300

    Add files via upload

---
+++ DIO/Estrutura Condicional switch case/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("informe um numero de 1 até 7");
+        int option = scanner.nextInt();
+        sw
+
+
+
+
+
+    }
+}
